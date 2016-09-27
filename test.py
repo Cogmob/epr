0cb584aae8838c0d24ca4017015197bc56436fa2
cogmob <luke.avery@live.co.uk>
2016-09-27 14:29:06 +0100

    epr working

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from epr.epr_recursive_import import epr_recursive_import
+from epr.epr import epr
 
 def run():
     path = os.path.dirname(os.path.abspath(__file__))
@@ -16,8 +17,30 @@ def set_up(path):
     epr_recursive_import(path + '/tmp_example_data')
 
 def do_tests(path):
-    with open(path + '/tmp_example_data/yes01.py') as f:
-        if 'from epr import epr' not in f.read():
-            print('NOPE')
+    yes_tests = [
+            '/tmp_example_data/subdir/yes01.py',
+            '/tmp_example_data/yes01.py',
+            '/tmp_example_data/yes02.py',
+            '/tmp_example_data/yes03.py']
+
+    no_tests = [
+            '/tmp_example_data/subdir/no01.py',
+            '/tmp_example_data/no01',
+            '/tmp_example_data/no02.py',
+            '/tmp_example_data/no03.py']
+
+    for test in yes_tests:
+        with open(path + test) as f:
+            if 'from epr import epr' not in f.read():
+                epr('not working', 'red')
+            else:
+                epr('working', 'green')
+
+    for test in no_tests:
+        with open(path + test) as f:
+            if 'from epr import epr' in f.read():
+                epr('not working', 'red')
+            else:
+                epr('working', 'green')
 
 run()
