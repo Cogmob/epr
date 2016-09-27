2f46bd6ebc1c626f372ccd61154acd13d0c402b4
cogmob <luke.avery@live.co.uk>
2016-09-27 15:39:00 +0100

    correct packages

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         url='https://github.com/Cogmob/epr',
         description=('Tool for quickly adding debugging output'),
         packages=[
-            'epr.epr'
+            'epr'
         ],
         long_description=open('README.rst', 'r').read()
 )
